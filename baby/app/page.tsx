c8e0efdcc3821c6f64059e3572873c16cb35c80c
cgoing <neo.cgoing@gmail.com>
2025-03-26 23:15:22 +0900

    init friday

--- baby/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <h1 className="font-semibold text-9xl animate-pulse">babyAGENT.</h1>
-    </div>
-  );
-}
--- baby/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    position: 'bottom-right',
-  },
-};
-
-export default nextConfig;
